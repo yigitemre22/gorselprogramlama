a9f22056a2675f2357a680521454a3de8efa72d4
Yiğit Emre <emre2003yoruk@gmail.com>
2026-05-01 15:21:58 +0300

    guncelleme

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import pyodbc
 from PySide6.QtWidgets import QApplication, QMessageBox
-from ui_login import Login
+from login import Login
 
 def veritabani_kontrol():
     conn_str = (
